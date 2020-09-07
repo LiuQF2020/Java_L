702ba353247b3de8804d0dd02c21ea35ee85c652
LQF <liuqifeng19960918@outlook.com>
2020-09-07 11:21:46 +0800

    二叉树、comparable存放数据

--- ALiyunDX/src/Tree_L/BinaryTree_L/JavaBinaryTree.java
+++ ALiyunDX/src/Tree_L/BinaryTree_L/JavaBinaryTree.java
@@ -1,5 +1,23 @@
 package Tree_L.BinaryTree_L;
 
 public class JavaBinaryTree {
+    public static void main(String[] args) {
 
+    }
+}
+
+/**
+ * 实现二叉树的操作
+ * @param <T> 要进行二叉树的实现
+ */
+class BinaryTree<T extends Comparable<T>> {
+    private class Node {
+        private Comparable<T> data; // 存放Comparable，可以比较大小
+        private Node parent; // 存父节点
+        private Node left; // 保存左子树
+        private Node right; // 保存右子树
+        public Node(Comparable<T> data) { // 构造方法直接负责数据的存储
+            this.data = data;
+        }
+    }
 }
